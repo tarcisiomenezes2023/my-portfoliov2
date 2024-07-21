d056cb964514b8466b002e30aa39b5ec9faa99a1
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-07-20 22:14:45 -0300

    Navbar ready, next is the ToggleButton

--- portfolio-v2/src/components/Navbar.jsx
+++ portfolio-v2/src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
       <div className="wrapper">
         <span>Tarcísio Menezes</span>
         <div className="social">
-          <a href="#"><img src="/img/github1.png" alt="Github" /></a>
-          <a href="#"><img src="/img/linkedin1.png" alt="Linkedin" /></a>
-          <a href="#"><img src="/img/instagram1.png" alt="Instagram" /></a>
-          <a href="#"><img src="/img/facebook2.png" alt="Facebook" /></a>
-          <a href="#"><img src="/img/outlook1.png" alt="Outlook" /></a>
+          <a href="https://github.com/tarcisiomenezes2023" target="blank"><img src="/img/github1.png" alt="Github" /></a>
+          <a href="https://www.linkedin.com/in/tarc%C3%ADsio-menezes-03b8a0233/" target="blank"><img src="/img/linkedin1.png" alt="Linkedin" /></a>
+          <a href="https://www.instagram.com/tarcisio.menezes19/" target="blank"><img src="/img/instagram1.png" alt="Instagram" /></a>
+          <a href="https://www.facebook.com/profile.php?id=100092629518329" target="blank"><img src="/img/facebook2.png" alt="Facebook" /></a>
+          <a href="mailto:tarcisiomenezes2019@hotmail.com" target="blank"><img src="/img/outlook1.png" alt="Email" /></a>
         </div>
       </div>
     </div>
