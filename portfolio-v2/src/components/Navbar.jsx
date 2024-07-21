5a22e947f2d6df16945e39ec2dc3eb8f07811812
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-07-20 21:43:58 -0300

    .

--- portfolio-v2/src/components/Navbar.jsx
+++ portfolio-v2/src/components/Navbar.jsx
@@ -2,9 +2,19 @@ import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-   <div className="navbar">
-    {/* Sidebar */}
-   </div>
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Tarcísio Menezes</span>
+        <div className="social">
+          <a href="#"><img src="/img/github1.png" alt="Github" /></a>
+          <a href="#"><img src="/img/linkedin1.png" alt="Linkedin" /></a>
+          <a href="#"><img src="/img/instagram1.png" alt="Instagram" /></a>
+          <a href="#"><img src="/img/facebook2.png" alt="Facebook" /></a>
+          <a href="#"><img src="/img/outlook1.png" alt="Outlook" /></a>
+        </div>
+      </div>
+    </div>
   );
 };
 
