258ed6dbc362c51367cb7b7b3e73b6759222795a
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-08-02 15:38:06 -0300

    Progress bar needing fix bugs

--- portfolio-v2/src/components/Portfolio.jsx
+++ portfolio-v2/src/components/Portfolio.jsx
@@ -1,12 +1,14 @@
+import { useRef } from "react";
 import "./Portfolio.scss"
-
+import { motion, useScroll, useSpring } from "framer-motion";
+import PropTypes from 'prop-types';
 
 const items = [
   {
     id: 1,
-    title: "Apollo",
+    title: "Working on right now...",
     img: "./img/ApolloAI.png",
-    description: "Apollo is a Environmental Engineering AI is an innovative ChatGPT-based assistant designed specifically for university students. This AI leverages cutting-edge natural language processing to provide real-time support on a wide range of environmental engineering topics.",
+    description: "Apollo is an Environmental Engineering AI, an innovative ChatGPT-based assistant designed specifically for university students. This AI leverages cutting-edge natural language processing to provide real-time support on a wide range of environmental engineering topics.",
   },
   {
     id: 2,
@@ -22,21 +24,49 @@ const items = [
   }
 ];
 
-const Single = ({item}) => {
+const Single = ({ item }) => {
   return (
     <section>
-      {item.title}
+      <h2>{item.title}</h2>
+      <img src={item.img} alt={item.title} />
+      <p>{item.description}</p>
     </section>
   )
 }
+
+Single.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end end"]
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
   return (
-    <div className="portfolio">
-      {items.map(item => (
-        <Single item={item} key={item.id}/>
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
       ))}
     </div>
   )
 }
 
-export default Portfolio
+export default Portfolio;
