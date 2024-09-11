8055d1016d71f36ce211b2374dac5164b22d39c2
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-09-11 19:12:08 +0200

    Updated CV

--- portfolio-v2/src/components/Hero.jsx
+++ portfolio-v2/src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
   });
 
   const handleDownloadCV = () => {
-    const CV = 'https://drive.google.com/file/d/16bManq_KUCw9gqjI1FZoGG2Ayxu6vh-A/view?usp=sharing';
+    const CV = 'https://drive.google.com/file/d/1YE5-FMXcaVACJMebVgIPDlsq9-sSQNA3/view?usp=sharing';
     window.open(CV, '_blank');
   }
 
